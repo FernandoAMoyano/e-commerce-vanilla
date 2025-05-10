af0c072e07924b783bc9fae4ff1af3b019e76fcf
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-10 20:10:10 -0300

    archivo products.js

---
+++ js/products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    title: "Curso de JavaScript",
+    price: 3600.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+  {
+    id: 2,
+    title: "Curso de Angular",
+    price: 2500.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+  {
+    id: 3,
+    title: "Curso de SQL",
+    price: 1200.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+  {
+    id: 4,
+    title: "Curso de React Js",
+    price: 5000.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+  {
+    id: 5,
+    title: "Curso de Dieseño",
+    price: 2000.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+  {
+    id: 6,
+    title: "Curso de Excel",
+    price: 1400.0,
+    image:
+      "https://www.solumex.com/wp-content/uploads/2013/11/dummy-image-square.jpg",
+  },
+];
